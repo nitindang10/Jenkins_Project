876033b1723aaa0313b873ac93b49646e822c1ff
nitin dang <nitindang333@gmail.com>
2022-01-10 14:28:19 +0530

    add file

---
+++ src/main/java/com/example/Jenkins_Docker/App.java
@@ -0,0 +1,22 @@
+package com.example.Jenkins_Docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    	
+	public static boolean cheackIfInputIsAnEvenNumber(int number)
+	{
+		return number % 2 == 0;
+	}
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(cheackIfInputIsAnEvenNumber(122));
+
+	}
+}
